12fc35cc3edbf4df08e1cc4bf1140921baba019d
Benjamin Porsont <porsont.benjamin@gmail.com>
2022-12-16 16:43:00 +0100

    modif

--- assets/scripts/toggle.js
+++ assets/scripts/toggle.js
@@ -1,6 +1,10 @@
 const BUTTON_ADD = document.getElementById("add"); // button to open the panel so we can create a new task
-const ARTICLE = document.querySelector("article")
+const ARTICLE = document.querySelector("article");
+const BUTTON_EXIT = document.getElementById("exit");
 
-let togglePanel = () => { ARTICLE.classList.toggle("displayed") }
+let togglePanel = () => {
+  ARTICLE.classList.toggle("displayed");
+};
 
-BUTTON_ADD.addEventListener('click', togglePanel)
+BUTTON_ADD.addEventListener("click", togglePanel);
+BUTTON_EXIT.addEventListener("click", togglePanel);
