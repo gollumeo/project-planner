0a896fe3dc6d0874f1ec2d04bfbdc8dbfe45533e
Pierre M <p.mauriello54@gmail.com>
2022-12-16 11:25:36 +0100

    arrays

---
+++ assets/scripts/panel.js
@@ -0,0 +1,9 @@
+const BUTTON_SUBMIT = document.getElementById("submit"); // button to "submit" the form to create a new task
+
+function getTask() {
+    let panelTitle = document.getElementById("panel-title");
+    let panelDescription = document.getElementById("panel-description");
+    let panelTaskStatus = document.getElementById("panel-task-status");
+    let panelAssignment = document.getElementById("panel-assignment");
+    let panelDueTime = document.getElementById("panel-due-time")
+}
